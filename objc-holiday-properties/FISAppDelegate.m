e6ba6cdf1f94097158174d19923521370d3f6f41
Elli Scharlin <escharlin@wesleyan.edu>
2016-06-08 18:05:36 -0400

    all tests are passing

--- objc-holiday-properties/FISAppDelegate.m
+++ objc-holiday-properties/FISAppDelegate.m
@@ -5,44 +5,64 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+//    [self holiday:@"Christmas Day" isInSeason:@"winter"];
     return YES;
 }
 
 - (NSArray *)holidaysInSeason:(NSString *)season {
     
-    return nil;
+    return self.database[season];
 }
 
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
                       inSeason:(NSString *)season {
     
-    return nil;
+    return self.database[season][holiday];
 }
 
 - (BOOL)holiday:(NSString* )holiday
      isInSeason:(NSString *)season {
-    
-    return nil;
+    BOOL isItInSeason = NO;
+    for (NSString *holidayStrings in self.database[season]) {
+        if ([holiday isEqualToString: holidayStrings]) {
+            isItInSeason = YES;
+        }
+    }
+    return isItInSeason;
 }
 
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
       inSeason:(NSString *)season {
-    
-    return nil;
+    BOOL isItInSeasonInHoliday = NO;
+    for (NSString *supplies in self.database[season][holiday]) {
+        if ([supply isEqualToString: supplies]) {
+            isItInSeasonInHoliday = YES;
+        }
+    }
+    return isItInSeasonInHoliday;
 }
 
 - (void)addHoliday:(NSString *)holiday
           toSeason:(NSString *)season {
     
-    // no return
+    NSMutableDictionary *databaseCopy = [self.database mutableCopy];
+    NSMutableArray *emptyArray = [NSMutableArray new];
+    [databaseCopy[season] setObject:emptyArray forKey:holiday ];
+    
+    self.database = databaseCopy;
+    
+
 }
 
 - (void)addSupply:(NSString *)supply
         toHoliday:(NSString *)holiday
          inSeason:(NSString *)season {
     
+    NSMutableDictionary *databaseCopy = [self.database mutableCopy];
+    [databaseCopy[season][holiday] addObject:supply];
+    
+    self.database = databaseCopy;
     // no return
 }
 
